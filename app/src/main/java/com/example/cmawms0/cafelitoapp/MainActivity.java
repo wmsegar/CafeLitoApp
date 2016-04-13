355cfe0263676ee2c81964b75d423ee8231500a6
wmsegar <wayne.segar@gmail.com>
2016-04-13 19:35:34 -0400

    Added Drink, Size, Name fields and Order button

--- app/src/main/java/com/example/cmawms0/cafelitoapp/MainActivity.java
+++ app/src/main/java/com/example/cmawms0/cafelitoapp/MainActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.cmawms0.cafelitoapp.data.CoffeeShop;
 import com.example.cmawms0.cafelitoapp.service.CoffeeShopService;
 import com.example.cmawms0.cafelitoapp.service.CoffeeShopServiceCallback;
+
 
 public class MainActivity extends Activity implements GoogleLocationService.LocationCallback, CoffeeShopServiceCallback{
 
@@ -18,6 +24,11 @@ public class MainActivity extends Activity implements GoogleLocationService.Loca
     private GoogleLocationService mGoogleLocationService;
     private TextView txtLatLong;
     private TextView txtNearestCoffeeshop;
+    private Spinner drinkSpinner;
+    private Spinner sizeSpinner;
+    private Button btnOrderCoffee;
+    private TextView txtName;
+    private EditText nameTextField;
     private CoffeeShopService service;
 
     @Override
@@ -26,6 +37,24 @@ public class MainActivity extends Activity implements GoogleLocationService.Loca
         setContentView(R.layout.activity_main);
         txtLatLong = (TextView) findViewById(R.id.txtLatLong);
         txtNearestCoffeeshop = (TextView) findViewById(R.id.txtNearestCoffeeshop);
+        txtName = (TextView) findViewById(R.id.txtName);
+        nameTextField = (EditText) findViewById(R.id.nameTextField);
+
+        //Bind Drink values to Spinner
+        drinkSpinner = (Spinner) findViewById(R.id.drinkSpinner);
+        ArrayAdapter<CharSequence> drinkAdapter = ArrayAdapter.createFromResource(this,
+                R.array.drink_array, android.R.layout.simple_spinner_dropdown_item);
+        drinkAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        drinkSpinner.setAdapter(drinkAdapter);
+
+        //Bind Size values to Spinner
+       sizeSpinner = (Spinner) findViewById(R.id.sizeSpinner);
+        ArrayAdapter<CharSequence> sizeAdapter = ArrayAdapter.createFromResource(this,
+               R.array.size_array, android.R.layout.simple_spinner_dropdown_item);
+        sizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sizeSpinner.setAdapter(sizeAdapter);
+
+        //Create new instance of GoogleLocationService
         mGoogleLocationService = new GoogleLocationService(this, this);
     }
 
@@ -52,11 +81,16 @@ public class MainActivity extends Activity implements GoogleLocationService.Loca
 
     @Override
     public void serviceSuccess(CoffeeShop coffeeShop) {
-        txtNearestCoffeeshop.setText(coffeeShop.getName());
+
+        txtNearestCoffeeshop.setText("Your coffee will be ready for pick up at: " + coffeeShop.getName());
     }
 
     @Override
     public void serviceFailure(Exception exception) {
         txtNearestCoffeeshop.setText("Couldn't get CoffeeShop");
+    }
+
+    public void submitCoffee (View view){
+
     }
 }
